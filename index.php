f75d6e43ec08f4c266af22656ad4355baab6344e
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-01 11:13:38 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body {
+				background-color: maroon;
+			}
+		</style>			
+	</head>
+	<body class="game">
+		<h1>Rock ~ Paper ~ Scissors</h1>
+		<h2>Choose your champion!</h2>
+	<form>
+		<input type="button" value="Rock">
+		<input type="button" value="Paper">
+		<input type="button" value="Scissors">
+		<br>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+	<script></script>
+	</body>
+
+</html>
